1ece5bc0181924358ca50886c6e5d9a64b4e4844
Juan Ceballos <juandavi789@hotmail.com>
2024-03-06 23:12:26 -0500

    Add styles to the main component, a piece of state for storing the app data, and reder a component base on that data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { useState } from 'react';
 import SearchBar from './components/SearchBar';
+import GiphyTable from './components/GiphyTable';
 
 const App = () => {
+  const [data, setData] = useState(null);
+
   return (
-    <div className='bg-gray-700 flex flex-col justify-center items-center h-screen'>
-      <main className='bg-gray-900 container max-w-5xl shadow-2xl rounded-lg p-10'>
-        <SearchBar />
+    <div className='flex h-screen flex-col items-center justify-center bg-gray-700'>
+      <main className='container max-w-5xl rounded-lg bg-gray-900 p-10 shadow-2xl'>
+        <SearchBar setData={setData} />
+        {data && <GiphyTable data={data} />}
       </main>
     </div>
   );
